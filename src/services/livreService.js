2d454c5cf74744b8f9cbd54950b7978185cd295b
sh <sandrahammamitlili@gmail.com>
2024-05-23 18:29:44 +0200

    config-commit

--- src/services/livreService.js
+++ src/services/livreService.js
@@ -2,7 +2,9 @@ const LIVRE_API="/livres/"
 
 export const fetchLivres=async()=> { 
 const res = await fetch(process.env.NEXT_PUBLIC_API_URL+LIVRE_API, { cache: 'no-store' })
+
 const response = await res.json();
+console.log(response)
 return response;
 }
 
